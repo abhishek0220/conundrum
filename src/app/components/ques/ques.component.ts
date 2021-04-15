bf67cfd3187660c85fca3c3df90108c253e4d81c
Abhishek Chaudhary <43341786+abhishek0220@users.noreply.github.com>
2021-04-15 08:32:24 +0000

    auto fetch ques

--- src/app/components/ques/ques.component.ts
+++ src/app/components/ques/ques.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StoreInfoService }  from '../../services/store-info.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { HttpClient } from '@angular/common/http';
 import { Router } from "@angular/router";
 
@@ -10,10 +11,13 @@ import { Router } from "@angular/router";
   styleUrls: ['./ques.component.css']
 })
 export class QuesComponent implements OnInit {
-
+  ques: any = {};
+  ansForm: FormGroup;
+  progress = true;
   constructor(
     private storeInfo: StoreInfoService,
     private http: HttpClient,
+    public fb: FormBuilder,
     private snackBar: MatSnackBar,
     private router: Router
   ) { }
@@ -24,21 +28,65 @@ export class QuesComponent implements OnInit {
     }
     else{
       this.getQues();
+      this.mainForm();
     }
+  }
+  mainForm() {
+    this.ansForm = this.fb.group({
+      qno: ['', [Validators.required]],
+      ans: ['', [Validators.required]]
+    })
+  }
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
   logout(){
     this.storeInfo.reset();
     this.router.navigateByUrl('');
   }
   getQues(){
+    this.progress = true;
     var authHeader = {
       'Authorization' : this.storeInfo.authToken
     }
+    
     this.http.get(this.storeInfo.serverURL + '/ques', {headers : authHeader}).pipe().subscribe((data)=>{
       console.log(data)
+      this.ques = data;
+      this.ansForm.get('qno').setValue(data['SN'])
+      this.progress = false;
     },error =>{
       console.log(error)
+      this.progress = false;
     })
+  }
+  submitAns(){
+    if(!this.ansForm.valid){
+      this.openSnackBar("Invalid Input","Close")
+      return false;
+    }
+    else{
+      var authHeader = {
+        'Authorization' : this.storeInfo.authToken
+      }
+      this.progress = true;
+      this.http.post(this.storeInfo.serverURL + '/submit', this.ansForm.value, {headers : authHeader}).pipe().subscribe((data)=>{
+        console.log(data)
+        if(data['status'] != 201){
+          this.openSnackBar(data['msg'] || data['message'],"Close")
+        }
+        else{
+          this.ansForm.get('ans').setValue("");
+          this.getQues();
+        }
+        this.progress = false;
+      },error =>{
+        console.log(error)
+        this.progress = false;
+      })
+    }
   }
 
 }
