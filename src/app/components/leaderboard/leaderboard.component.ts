550ccafaf3b2e0d082699e56b72d577b96351b69
Abhishek Chaudhary <43341786+abhishek0220@users.noreply.github.com>
2021-04-17 03:43:23 +0000

    check

--- src/app/components/leaderboard/leaderboard.component.ts
+++ src/app/components/leaderboard/leaderboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StoreInfoService }  from '../../services/store-info.service';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-leaderboard',
@@ -9,18 +10,30 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LeaderboardComponent implements OnInit {
   leaderBoard = [];
+  typeStr = "";
+  toDisp = 0;
   constructor(
     private http: HttpClient,
     private storeInfo: StoreInfoService,
-  ) { }
+    private activatedroute: ActivatedRoute,
+  ) {
+    this.activatedroute.queryParamMap.subscribe(params => {
+      var tmp = params.get('name') || 'null';
+      this.typeStr = tmp;
+    });
+  }
 
   ngOnInit(): void {
     this.getRanklist()
   }
   getRanklist(){
-    this.http.get(this.storeInfo.serverURL + '/leaderboard').pipe().subscribe((data)=>{
+    this.http.get(this.storeInfo.serverURL + '/leaderboard' + `?name=${this.typeStr}`).pipe().subscribe((data)=>{
       console.log(data)
-      if(data['status'] == 200) this.leaderBoard = data['list']
+      if(data['status'] == 200){
+        this.leaderBoard = data['list'];
+        this.toDisp = 1;
+      }
+      else this.toDisp = 2;
     },error =>{
       console.log(error)
     })
